6d4d86115eba79184443fb417f58de3ef2cd9e2d
baobaotql <40971441+baobaotql@users.noreply.github.com>
2019-10-22 16:21:23 +0800

    Update maze.py

--- Task2/maze.py
+++ Task2/maze.py
@@ -1,22 +1,33 @@
-# -*- coding: utf-8 -*-
+ -*- coding: utf-8 -*-
 # @Time : 2019/10/19 10:26
 # @Author : BaoBao
 # @Mail : baobaotql@163.com
 # @File : mzae.py
 # @Software: PyCharm
+import numpy as np
 
-maze = [
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    [1, 0, 0, 0, 0, 0, 0, 1, 0, 1],
-    [1, 1, 0, 1, 1, 1, 1, 1, 0, 1],
-    [1, 0, 1, 0, 0, 1, 0, 0, 1, 1],
-    [1, 0, 0, 1, 1, 0, 1, 0, 1, 1],
-    [1, 0, 1, 0, 0, 0, 1, 1, 0, 1],
-    [1, 0, 1, 1, 1, 1, 1, 0, 1, 1],
-    [1, 0, 0, 1, 1, 1, 0, 1, 1, 1],
-    [1, 1, 1, 0, 0, 1, 0, 0, 0, 1],
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
-]
+maze =np.array ([
+    [0, 0, 0, 0, 0, 0, 1, 0],
+    [1, 0, 1, 1, 1, 1, 1, 0],
+    [0, 1, 0, 0, 1, 0, 0, 1],
+    [0, 0, 1, 1, 0, 1, 0, 1],
+    [0, 1, 0, 0, 0, 1, 1, 0],
+    [0, 1, 1, 1, 1, 1, 0, 1],
+    [0, 0, 1, 1, 1, 0, 1, 1],
+    [1, 1, 0, 0, 1, 0, 0, 0],
+])
+
+p = np.array([1, 1, 1, 1, 1, 1, 1, 1])
+q = np.array([1, 1, 1, 1, 1, 1, 1, 1, 1, 1])
+
+a = np.insert(maze, 0, values=p, axis=1)
+b = np.insert(a, 9,values=p, axis=1)
+
+c = np.insert(b, 0, values=q, axis=0)
+d = np.insert(c, 9, values=q, axis=0)
+
+print("初始化迷宫：")
+print(d)#为解决越界问题 初始化迷宫
 
 dirs = [lambda x, y: (x + 1, y),
         lambda x, y: (x - 1, y),
@@ -31,22 +42,24 @@ dirs = [lambda x, y: (x + 1, y),
 def mpath(x1, y1, x2, y2):
     stack = []      #建立抽象栈
     stack.append((x1, y1))      #加入初始点
-    maze[x1][y1] = -1       #表示已经走过
+    d[x1][y1] = -1       #表示已经走过
     while len(stack) > 0:
         curNode = stack[-1]
-        if curNode[0] == x2 and curNode[1] == y2:   #到达终点
-            for p in stack:
-                print(p)
+        if curNode == (x2, y2):
+            print(stack)
             return True
+
         for dir in dirs:
             nextNode = dir(curNode[0], curNode[1])
-            if maze[nextNode[0]][nextNode[1]] == 0 : #找到了下一个
+            if d[nextNode[0]][nextNode[1]] == 0 : #找到了下一个
                 stack.append(nextNode)
-                maze[nextNode[0]][nextNode[1]] = -1  # 标记为已经走过，防止死循环
+                d[nextNode[0]][nextNode[1]] = -1  # 标记为已经走过，防止死循环
                 break
+
         else:   #八个方向都没找到
-            maze[curNode[0]][curNode[1]] = -1  # 死路一条,标记下次不走
-            stack.pop()     #回溯
+            d[curNode[0]][curNode[1]] = -1  # 死路一条,标记下次不走
+            stack.pop()  #回溯
+
     print("没有路")
     return False
 
