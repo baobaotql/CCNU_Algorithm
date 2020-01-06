2494efb72edd6141f5da1ebbe7a3c1ef9cf07a21
baobaotql <40971441+baobaotql@users.noreply.github.com>
2020-01-06 13:53:40 +0800

    PAT test

--- PAT/A1025.py
+++ PAT/A1025.py
@@ -1,7 +1,6 @@
 //A1025
-#include<stdio.h>
-#include<string.h>
-#include<stdlib.h>
+#include<cstdio>
+#include<cstring>
 #include<algorithm>
 using namespace std;
 
@@ -11,7 +10,7 @@ struct Student
 	int score;
 	int location_number;
 	int local_rank;
-}stu[300];
+}stu[30010];
 bool cmp(Student a,Student b)
 {
 	if(a.score!=b.score)
@@ -31,38 +30,38 @@ int main()
 	for(int i=1;i<=n;i++)
 	{
 		scanf("%d",&k);
-			for(int j=0;j<k;j++)
-			{
-				scanf("%s %d",stu[num].id,&stu[num].score);
-				stu[num].location_number=i;
-				num++;
-			}
-			sort(stu+num-k,stu+num,cmp);
-			stu[num-k].local_rank=1;
-			for(int j=num-k+1;j<num;j++)
-			{
-				if(stu[j].score==stu[j-1].score)
-				{
-					stu[j].local_rank==stu[j-1].local_rank;
-				}
-				else
-				{
-					stu[j].local_rank=j+1-(num-k);
-				}
-			}
-	}
-		printf("%d\n",num);
-		sort(stu,stu+num,cmp);
-		int r=1;
-		for(int i=0;i<num;i++)
+		for(int j=0;j<k;j++)
 		{
-			if(i>0&&stu[i].score!=stu[i-1].score)
-			{
-				r=i+1;
-			}
-			printf("%s ",stu[i].id);
-			printf("%d %d %d\n",r,stu[i].location_number,stu[i].local_rank);
+			scanf("%s %d",stu[num].id,&stu[num].score);
+			stu[num].location_number=i;
+			num++;
 		}
+		sort(stu+num-k,stu+num,cmp);
+		stu[num-k].local_rank=1;
+		for(int j=num-k+1;j<num;j++)
+		{
+			if(stu[j].score==stu[j-1].score)
+			{
+				stu[j].local_rank==stu[j-1].local_rank;
+			}
+			else
+			{
+				stu[j].local_rank=j+1-(num-k);
+			}
+		}
+	}
+	printf("%d\n",num);
+	sort(stu,stu+num,cmp);
+	int r=1;
+	for(int i=0;i<num;i++)
+	{
+		if(i>0&&stu[i].score!=stu[i-1].score)
+		{
+			r=i+1;
+		}
+		printf("%s ",stu[i].id);
+		printf("%d %d %d\n",r,stu[i].location_number,stu[i].local_rank);
+	}
 	return 0;
 }
  
