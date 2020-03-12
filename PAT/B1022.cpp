7cc12ba450be4b6d89d64354bdd615d61844dbbc
baobao <baobaotql@163.com>
2020-03-12 17:39:47 +0800

    update B1022

--- PAT/B1022.cpp
+++ PAT/B1022.cpp
@@ -1,21 +1,21 @@
-//B1022
+//PAT B1022
 #include<stdio.h>
 
-int main()
+int main() 
 {
-	int a,b,d;
-	scanf("%d %d %d",&a,&b,&d);
-	int sum=a+b;
-	
-	int ans[31],num=0;
+	int a, b, d;
+	scanf("%d%d%d", &a, &b, &d);
+	int sum = a+ b;
+	int ans[31], num = 0;
 	do
 	{
-		ans[num++]=sum%d;
-		sum/=d;
-	}while(sum!=0);
-	for(int i=num-1;i>=0;i--)
+		ans[num++] = sum % d;
+		sum = sum / d; 
+	}while(sum != 0);
+	for(int i = num - 1; i >= 0; i--)
 	{
-		printf("%d",ans[i]);
+		printf("%d", ans[i]);
 	}
+	printf("%d",1 % 2);
 	return 0;
 }
