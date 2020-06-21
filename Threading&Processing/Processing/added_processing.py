f6d1f5b3d2c422ee15e757864fc46d04955d8110
baobao <baobaotql@163.com>
2020-06-21 22:44:53 +0800

    创建进程

---
+++ Threading&Processing/Processing/added_processing.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*- 
+# @Time : 2020/5/7 16:21 
+# @Author : BaoBao
+# @Mail : baobaotql@163.com 
+# @File : added_processing.py 
+# @Software: PyCharm
+
+import multiprocessing as mp
+
+def job(a,d):
+    print('aaaaa')
+
+if __name__=='__main__':
+    p1 = mp.Process(target=job,args=(1,2))
+    p1.start()
+    p1.join()
