100fba10ea19a96178426d973fc4c895ae0dea7b
baobao <baobaotql@163.com>
2020-06-21 22:46:57 +0800

    队列测试

---
+++ Threading&Processing/Threading/queue_test.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*- 
+# @Time : 2020/5/7 11:11 
+# @Author : BaoBao
+# @Mail : baobaotql@163.com 
+# @File : queue_test.py
+# @Software: PyCharm
+
+import threading
+import time
+from queue import Queue
+
+def job(l,q):
+    for i in range(len(l)):
+        l[i] = l[i]**2
+    q.put(l)
+
+def multithreading():
+    q = Queue()
+    threads = []
+    data = [[1,2,3],[3,4,5],[4,4,4],[5,5,5]]
+    for i in range(4):
+        t = threading.Thread(target=job, args=(data[i], q))
+        t.start()
+        threads.append(t)
+    for thread in threads:
+        thread.join()
+    results = []
+    for _ in range(4):
+          results.append(q.get())
+    print(results)
+
+if __name__ == '__main__':
+    multithreading()
