66d710d046a1df70ff5232e2feb90c2981aa115e
baobao <baobaotql@163.com>
2020-06-25 22:21:47 +0800

    test

--- PAT/test.cpp
+++ PAT/test.cpp
@@ -1,180 +1,51 @@
 #include<stdio.h>
+#include<string.h>
+#include<algorithm>
+using namespace std;
 
-/*
-//字符串是否出现 
-const int maxn = 100;
-char S[maxn][5], temp[5];
-int hashTable[26*26*26 + 10];
-
-int hashFunc(char S[], int len)
+struct mooncake
 {
-	int id = 0;
-	for(int i = 0; i < len; i++)
-	{
-		id = id * 26 + (S[i] - 'A');
-	}
-	return id;
-}
+	double store;
+	double sell;
+	double price;
+ } cake[1000];
 
+bool cmp(mooncake a, mooncake b)
+{
+	return a.price > b.price;
+ } 
 int main()
 {
-	int n, m;
-	scanf("%d%d", &n, &m);
-	
-	for(int i = 0; i < m; i++)
+	int n;
+	double D;
+	scanf("%d%lf", &n, &D);
+	for(int i=0; i<n; i++)
 	{
-		scanf("%s", temp);
-		int id = hashFunc(S[i], 3);
-		hashTable[id]++;
-	}
-	for(int i = 0; i < m; i++)
-	{
-		scanf("%s", temp);
-		int id = hashFunc(temp, 3);
-		printf("%d\n", hashTable[id]);
-	}
-	return 0;
+		scanf("%lf", &cake[i].store);
+	 } 
+	 for(int i=0; i<n; i++)
+	 {
+	 	scanf("%lf", &cake[i].sell);
+	 	cake[i].price = cake[i].sell / cake[i].store;
+	 }
+	 
+	 sort(cake, cake + n, cmp);
+	 
+	 double ans = 0;
+	 
+	 for(int i = 0; i < n; i++)
+	 {
+	 	if(cake[i].store <= D)
+	 	{
+	 		D = D - cake[i].store;
+	 		ans = ans + cake[i].sell;
+		 }
+		 else
+		 {
+		 	ans = ans + cake[i].price * D;
+		 	break;
+		 }
+	 }
+	 printf("%.2f\n", ans);
+	 return 0;
 }
-*/
-/*
-//数字是否出现 
-const int maxn = 100010;
-bool hashTable[maxn] = {false};
-
-int main()
-{
-	int n, m, x;
-	scanf("%d%d", &n, &m);
-	for(int i = 0; i < n; i++)
-	{
-		scanf("%d", &x);
-		hashTable[x] = true;
-	}
-	for(int i = 0; i < m; i++)
-	{
-		scanf("%d", &x);
-		if(hashTable[x] == true)
-		{
-			printf("yes\n");
-		}
-		else
-		{
-			printf("No\n");
-		}
-	}
-	return 0;
-}
-*/
-/*
-//数字出现统计 
-const int maxn = 100010;
-int hashTable[maxn] = {0};
-
-int main() 
-{
-	int n, m, x;
-	scanf("%d%d", &n, &m);
-	for(int i = 0; i < n; i++)
-	{
-		scanf("%d", &x);
-		hashTable[x]++;
-	}
-	for(int i = 0; i < m; i++)
-	{
-		scanf("%d", &x);
-		printf("%d\n", hashTable[x]);
-	}
-	return 0;
-}
-*/
-
-//递归 全排列
-const int maxn = 11;
-int n, P[maxn], hashTable[maxn] = {false};
-
-void generateP(int index)
-{
-	if(index == n + 1)
-	{
-		for(int i = 1; i <= n; i++)
-		{
-			printf("%d", P[i]);
-		}
-		printf("\n");
-		return;
-	}
-	for(int x = 1; x <= n; x++)
-	{
-		if(hashTable[x] == false)
-		{
-			P[index] = x;
-			hashTable[x] = true;
-			generateP(index + 1);
-			hashTable[x] = false;
-		}
-	}
-} 
-int main()
-{
-	n = 3;
-	generateP(1);
-	return 0;
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
