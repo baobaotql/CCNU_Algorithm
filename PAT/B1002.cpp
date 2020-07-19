9d7d828e51cf9cbffeeda7bd7634456f122eb15d
baobao <baobaotql@163.com>
2020-07-20 00:30:26 +0800

    Update B1002.cpp

--- PAT/B1002.cpp
+++ PAT/B1002.cpp
@@ -1,24 +1,28 @@
 #include<iostream>
 #include<string>
 using namespace std;
+
 string s;
 string num[10] = { "ling","yi","er","san","si","wu","liu","qi","ba","jiu" };
+
 int main()
 {
     getline(cin, s);
     int ans = 0;
     for (int i = 0; i < s.length(); i++)
     {
-        ans +=s[i] - '0';
+        ans +=s[i] - '0';	//数组转数字 
     }
     int a[100], cnt = 0;
-    do {
+    do 
+	{
         a[cnt++] = ans % 10;
         ans /= 10;
     } while (ans);
+    
     for (int i = cnt - 1; i >= 0; i--)
     {
-        cout << num[a[i]];
+        printf("%s",num[a[i]]); 
         if (i)printf(" ");
     }
     return 0;
