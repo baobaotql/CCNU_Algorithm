152ca630e6960c70433ed04b02084a3086d2e9f8
baobao <baobaotql@163.com>
2020-07-21 23:49:15 +0800

    Create set_eraser(it).cpp

---
+++ PAT/STL/set/set_eraser(it).cpp
@@ -0,0 +1,21 @@
+//eraser
+//É¾³ıµ¥¸öÔªËØ 
+#include<stdio.h>
+#include<set>
+using namespace std;
+
+int main()
+{
+	set<int> st;
+	st.insert(100);
+	st.insert(200);
+	st.insert(100);
+	st.insert(300);
+	st.erase(st.find(100));
+	st.erase(st.find(200));
+	for(set<int>::iterator it = st.begin(); it != st.end(); it++)
+	{
+		printf("%d\n", *it);
+	}
+	return 0;
+}
