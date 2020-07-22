b2580d0ba05aecc3fa2baad09128f068b38566b0
baobao <baobaotql@163.com>
2020-07-22 23:10:55 +0800

    Create set_size.cpp

---
+++ PAT/STL/set/set_size.cpp
@@ -0,0 +1,16 @@
+//size
+#include<stdio.h>
+#include<set>
+using namespace std;
+
+int main() 
+{
+	set<int> st;
+	st.insert(2);
+	st.insert(5);
+	st.insert(4);
+	st.clear();
+	
+	printf("%d", st.size());
+	return 0;
+}
