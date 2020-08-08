2759f16cc3a306f6522a37a9c6dba2f75d76afe5
baobao <baobaotql@163.com>
2020-08-08 20:56:07 +0800

    Create stack_top.cpp

---
+++ PAT/STL/stack/stack_top.cpp
@@ -0,0 +1,15 @@
+//top
+#include<stdio.h>
+#include<stack>
+using namespace std;
+
+int main()
+{
+	stack<int> st;
+	for(int i = 1; i <= 5; i++)
+	{
+		st.push(i);
+	}
+	printf("%d\n", st.top());
+	return 0;
+ } 
