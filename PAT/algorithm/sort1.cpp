5ac0af4c4c63ad4456b97b62d233b2dbb13f3671
baobao <baobaotql@163.com>
2020-08-16 21:42:48 +0800

    Create sort1.cpp

---
+++ PAT/algorithm/sort1.cpp
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+bool cmp(int a, int b)
+{
+	return  a > b;
+}
+int main()
+{
+	vector<int> vi;
+	vi.push_back(3);
+	vi.push_back(1);
+	vi.push_back(2);
+	sort(vi.begin(), vi.end(), cmp);
+	for(int i = 0; i < 3; i++)
+	{
+		printf("%d ", vi[i]);
+	}
+	return 0;
+}
